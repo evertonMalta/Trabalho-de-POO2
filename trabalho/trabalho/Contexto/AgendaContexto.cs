45be0d34b0ce6b304361d23b982eb7973c4d37b8
evertonMalta <everton.malta.7@gmail.com>
2018-11-07 01:47:10 -0200

    primeiro commit

---
+++ trabalho/trabalho/Contexto/AgendaContexto.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trabalho.Contexto
+{
+    class AgendaContexto: DbContext
+    {
+        public AgendaContexto() : base(nameOrConnectionString: "agenda")
+        {
+            Database.SetInitializer<AgendaContexto>(new AgendaContextoInitialize());        
+        }
+        public DbSet<Pessoa> Pesssoas { get; set; }
+        public class AgendaContextoInitialize : DropCreateDatabaseAlways<AgendaContexto>
+        {
+
+        }
+    }
+    
+}
---
+++ trabalho/trabalho/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using trabalho.Contexto;
+
+namespace trabalho
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void Salvar(object sender,RoutedEventArgs e)
+        {
+            Pessoa contato = new Pessoa()
+            {
+                Name = "seu zé",
+                Numero = "2345678"
+            };
+            using(var contexto= new AgendaContexto()){
+                contexto.Pesssoas.Add(contato);
+                contexto.SaveChanges();
+            }
+        }
+    }
+}
